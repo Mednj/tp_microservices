3c21a58d336d94eb173117bd6356e7ca3bdda272
Med Nj <razerhazer.2020@gmail.com>
2023-12-09 23:48:55 +0100

    first commit

---
+++ client/client/src/main/java/com/naja/client/entities/Client.java
@@ -0,0 +1,24 @@
+package com.naja.client.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String nom;
+    private String prenom;
+    private int age;
+}
---
+++ client/client/src/main/java/com/naja/client/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.naja.client.repositories;
+
+import com.naja.client.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
